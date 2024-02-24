d00a809171ed3bfdc38628fbacdf67da46d401aa
adrn-mm <adrian.m.muhammad@gmail.com>
2024-02-25 06:58:59 +0700

    change code and rename files in face recognition folder

--- face_recognition/predict.py
+++ face_recognition/predict.py
@@ -3,15 +3,22 @@ import os
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
+import subprocess
 
 # Fungsi untuk menampilkan teks pada gambar
 def put_text(test_img, text, x, y):
     cv2.putText(test_img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-# Lokasi model dan gambar
-model_path = 'bdktrainer.yml'
-image_path = 'foto_bersama.jpg'
+# Lokasi model dan gambar di HDFS
+hdfs_image_path = '/user/csso_adrian.muhammad/images/predict.jpg'
 casc_path = 'haarcascade_frontalface_default.xml'
+
+# Lokasi sementara di lokal
+local_model_path = 'trainer.yml'
+local_image_path = '/tmp/predict.jpg'
+
+# Download gambar dari HDFS
+subprocess.run(['hdfs', 'dfs', '-get', hdfs_image_path, local_image_path], check=True)
 
 # Nama untuk setiap label
 name = {0: "Anies", 1: "Prabowo", 2: "Ganjar"}
@@ -19,10 +26,10 @@ name = {0: "Anies", 1: "Prabowo", 2: "Ganjar"}
 # Memuat detektor wajah dan model pengenalan wajah
 detector = cv2.CascadeClassifier(cv2.data.haarcascades + casc_path)
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read(model_path)
+recognizer.read(local_model_path)
 
 # Memuat dan memproses gambar
-img = cv2.imread(image_path)
+img = cv2.imread(local_image_path)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Mendeteksi wajah dalam gambar
@@ -43,3 +50,6 @@ for (x, y, w, h) in faces:
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 plt.imshow(img_rgb)
 plt.show()
+
+# Membersihkan file sementara lokal setelah digunakan
+os.remove(local_image_path)
--- face_recognition/training.py
+++ face_recognition/training.py
@@ -1,51 +1,47 @@
 import cv2
-import os 
-from PIL import Image, ImageDraw
+import os
+import subprocess
+from PIL import Image
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.image import imread
 
-image_path = os.path.join(os.getcwd(), 'image_raw_data')
+# Lokasi folder di HDFS
+hdfs_image_path = '/user/csso_adrian.muhammad/images/image_training'
 
-casc_path = os.path.join(os.getcwd(), 'haarcascade_frontalface_default.xml')
+# Lokasi sementara di lokal untuk menyimpan gambar yang diunduh
+local_image_path = '/tmp/image_training'
 
-detector = cv2.CascadeClassifier(casc_path)
+# Mengunduh folder gambar dari HDFS
+subprocess.run(['hadoop', 'fs', '-get', hdfs_image_path, local_image_path], check=True)
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-image_full_list = ['0.1', '0.2', '0.3', '0.4', '0.5',
-                 '1.1', '1.2', '1.3', '1.4', '1.5',
-                 '2.1', '2.2', '2.3', '2.4', '2.5']
+# Path ke file cascade untuk deteksi wajah
+casc_path = 'haarcascade_frontalface_default.xml'
+detector = cv2.CascadeClassifier(cv2.data.haarcascades + casc_path)
 
 def getImagesAndLabels(path):
-    imagePaths = [os.path.join(path,f) for f in os.listdir(path)] 
-    faceSamples=[]
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faceSamples = []
     ids = []
     for imagePath in imagePaths:
-        PIL_img = Image.open(imagePath).convert('L') #Luminance  ==> greystyle
-        img_numpy = np.array(PIL_img,'uint8')
+        PIL_img = Image.open(imagePath).convert('L')  # convert to grayscale
+        img_numpy = np.array(PIL_img, 'uint8')
         id = int(os.path.split(imagePath)[-1].split(".")[0])
         faces = detector.detectMultiScale(img_numpy)
-        if len(faces) > 1:
-            print("The following image detect more than 1 face", imagePath)
-        for (x,y,w,h) in faces:
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
+        for (x, y, w, h) in faces:
+            faceSamples.append(img_numpy[y:y+h, x:x+w])
             ids.append(id)
-            #print(ids)
-            
-    return faceSamples,ids
+    return faceSamples, ids
 
-faces,ids = getImagesAndLabels(image_path)
-print("{0} faces, {0} id in total are detected".format(len(faces), len(ids)))
-
-face_recognizer = cv2.face.LBPHFaceRecognizer_create() 
-face_recognizer.train(faces, np.array(ids))
+faces, ids = getImagesAndLabels(local_image_path)
+print(f"{len(faces)} faces, {len(ids)} ids in total are detected")
 
 def train_classifier(faces, faceID):
-    face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
     face_recognizer.train(faces, np.array(faceID))
     return face_recognizer
 
-# Save the model as bdktrainer.yml
-facerecognizer = train_classifier(faces, ids) 
-facerecognizer.save('bdktrainer.yml')
+# Train the classifier and save the model
+facerecognizer = train_classifier(faces, ids)
+facerecognizer.save('trainer.yml')
+
+# Hapus folder gambar sementara di lokal setelah digunakan
+subprocess.run(['rm', '-r', local_image_path])
